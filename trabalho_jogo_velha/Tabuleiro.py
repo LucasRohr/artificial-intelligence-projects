c2a2ad5bc3265896d662c753b689b29ebc2e1308
LucasRohr <lucasrc17@live.com>
2026-03-31 20:18:24 -0300

    feat: add Tabuleiro class for game assignment

---
+++ trabalho_jogo_velha/Tabuleiro.py
@@ -0,0 +1,66 @@
+class Tabuleiro:
+    def __init__(self):
+        # Matriz 3x3 para representar o tabuleiro do jogo da velha
+        self.tabuleiro = [[' ' for _ in range(3)] for _ in range(3)]
+
+    # Exibe o tabuleiro de forma visual
+    def exibir(self):
+        for linha in self.tabuleiro:
+            print('|'.join(linha))
+            print('-' * 5)
+    
+    # Gera uma lista de ações possíveis (posições vazias) no tabuleiro
+    # ACTIONS(state) -> list of actions
+    def gerar_acoes_possiveis(self):
+        acoes = []
+
+        for i in range(3):
+            for j in range(3):
+                if self.tabuleiro[i][j] == ' ':
+                    acoes.append((i, j))
+
+        return acoes
+    
+    # Verifica possibilidade de jogada e atualiza o tabuleiro com o respectivo jogador ('X' ou 'O')
+    # RESULT(state, action) -> new_state
+    def fazer_jogada(self, posicao, jogador):
+        i, j = posicao
+
+        if self.tabuleiro[i][j] == ' ':
+            self.tabuleiro[i][j] = jogador
+            return True
+        
+        return False
+    
+    # Checa por vencedor da partida
+    # TERMINAL(state) -> boolean
+    def verificar_vencedor(self):
+        # Checa linhas
+        for linha in self.tabuleiro:
+            if linha[0] == linha[1] == linha[2] != ' ':
+                return linha[0]
+            
+        # Checa colunas
+        for coluna in range(3):
+            if self.tabuleiro[0][coluna] == self.tabuleiro[1][coluna] == self.tabuleiro[2][coluna] != ' ':
+                return self.tabuleiro[0][coluna]
+
+        # Checa diagonais
+        if self.tabuleiro[0][0] == self.tabuleiro[1][1] == self.tabuleiro[2][2] != ' ':
+            return self.tabuleiro[0][0]
+
+        if self.tabuleiro[0][2] == self.tabuleiro[1][1] == self.tabuleiro[2][0] != ' ':
+            return self.tabuleiro[0][2]
+        
+        return None  # Nenhum vencedor
+    
+    # UTILITY(state, player) -> number
+    def calcular_utilidade(self, jogador):
+        vencedor = self.verificar_vencedor()
+
+        if vencedor == jogador:
+            return 1  # Se for o jogador passado, retorna 1 (vitória)
+        elif vencedor is not None:
+            return -1  # Se houver vencedor (not None) e não for o jogador passado, retorna -1 (derrota)
+        else:
+            return 0  # Se for empate ou jogo em andamento, retorna 0
